31d25c258f4677b7775be115aa97f60437dead7d
MrM <MMMohebi@outlook.com>
2022-06-26 15:12:39 +0430

    remove unused imports

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-import requests
 
 driver = webdriver.Chrome()
 driver.quit()
