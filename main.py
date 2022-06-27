b93a1b3f9fea05122226b5e46b87f6307896aa8f
MrM <MMMohebi@outlook.com>
2022-06-27 15:22:00 +0430

    tel bot add del users

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from config import ConfigGGG
 from golestanGradeGrabber import GolestanGradeGrabber
+from telegramBot import TelegramBotGGG
+
 
 if __name__ == '__main__':
     configs = ConfigGGG()
     ggg = GolestanGradeGrabber(configs.getLoginURL())
+    telBot = TelegramBotGGG(configs.getTelBotToken(), configs.getDB())
 
+
+    telBot.lessenAll()
+    telBot.getTelBot().infinity_polling()
---
+++ telegramBot.py
@@ -0,0 +1,50 @@
+import telebot
+import re
+
+
+class TelegramBotGGG:
+    TelBot = None
+    DB = None
+
+    def __init__(self, token, db):
+        self.TelBot = telebot.TeleBot(token, parse_mode=None)
+        self.DB = db
+
+    def getTelBot(self):
+        return self.TelBot
+
+    def lessenAll(self):
+        @self.TelBot.message_handler(func=lambda message: True)
+        def all_msgs(message):
+            if "addUser" in message.text:
+                userPass = list(filter(None, re.split(r"\n+|addUser", message.text)))
+                if userPass[0][:2] == "98":
+                    self.addUserToGroup(message.chat.id, userPass[0], userPass[1])
+
+            if "delUser" in message.text:
+                userPass = list(filter(None, re.split(r"\n+|delUser", message.text)))
+                if userPass[0][:2] == "98":
+                    self.removeUserFromGroup(message.chat.id, userPass[0])
+
+    def addUserToGroup(self, groupId, username, password):
+        isUserExist = self.DB["groups"].count_documents({"users": {"$elemMatch": {"username": username}}, "chatId": groupId}) != 0
+
+        if isUserExist:
+            self.TelBot.send_message(groupId, "exist! User " + username + " :)")
+            return
+
+        self.DB["groups"].update_one({"chatId": groupId}, {
+            "$push": {
+                "users": {"username": username, "password": password}
+            }
+        }, upsert=True)
+
+        self.TelBot.send_message(groupId, "submitted! User " + username + " :)")
+
+    def removeUserFromGroup(self, groupId, username):
+        self.DB["groups"].update_one({"chatId": groupId}, {
+            "$pull": {
+                "users": {"username": username}
+            }
+        })
+        self.TelBot.send_message(groupId, "deleted! User " + username + " :)")
