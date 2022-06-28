662fd3c1c42fa6383e8733d083e3fe6c1d8acf04
MrM <MMMohebi@outlook.com>
2022-06-28 11:32:01 +0430

    requirements

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from GolestanGradeGrabber import GolestanGradeGrabber
 
 if __name__ == '__main__':
     ggg = GolestanGradeGrabber()
-
+    print("started...")
     inter = SetInterval(3600, ggg.checkScores)
 
